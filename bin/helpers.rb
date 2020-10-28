4206eccc1b9e7b6df74b0000cf2e856077c1c654
kenderb <kenderbolivar@gmail.com>
2020-10-28 12:50:01 -0500

    add empty space validator

--- bin/helpers.rb
+++ bin/helpers.rb
@@ -17,7 +17,22 @@ module AuthenticatingValues
     end
     choice
   end
+
+  def self.entry_space_validator(board, value)
+    option = false
+    until option
+      if value == board[value - 1]
+        option = true
+      else
+        puts 'That spot is taken chose again'.red
+        new_input = gets.chomp
+        value = new_input.to_i
+        option = false
+      end
+    end
+    value
+  end
 end
 
-# value1 = gets.chomp
-# p AuthenticatingValues.numbers_validator(value1)
+# board = ["x", 2, 3, 4, 5, 6, 7, 8, 9]
+# p AuthenticatingValues.entry_space_validator(board, 1)
