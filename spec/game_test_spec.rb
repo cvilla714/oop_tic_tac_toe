c16a41a597a86769b549c8eb9e8f44258c613d20
Cosmel Villalobos <cosmelvg@gmail.com>
2020-11-03 15:21:28 -0500

    adding test for instructions and no instructions

--- spec/game_test_spec.rb
+++ spec/game_test_spec.rb
@@ -3,14 +3,29 @@ require_relative '../bin/main.rb'
 RSpec.describe GameUi do
   let(:maintitle) { GameUi.new('title', 'instructions') }
   let(:notitle) { GameUi.new(nil, 'instructions') }
+  let(:noinstuctions) { GameUi.new('title', 'No instructions') }
+  #   let(:firstplayer) { Player.new.ask_player_for_name('name', value) }
 
   it 'check if the title is displayed' do
     expect(maintitle.title).to eq('title')
   end
 
-     context "when you don't have a title" do
-     it 'check if the title is not displayed' do
-       expect(notitle.title).to eq('No title')
-        end
+  context "when you don't have a title" do
+    it 'check if the title is not displayed' do
+      expect(notitle.title).to eq('No title')
     end
+  end
+
+  describe 'when instructions are displayed' do
+    it 'check if the instructions are shown' do
+      expect(maintitle.instructions).to eq('instructions')
+    end
+
+    context 'when no instructions are shown' do
+      it 'will check if no instructions are available' do
+        expect(noinstuctions.instructions).to eq('No instructions')
+      end
+    end
+  end
+
 end
