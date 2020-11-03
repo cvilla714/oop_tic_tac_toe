ea8b09b52ef84ab4e91911365bb206ec191c0242
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2020-11-04 02:29:09 +0600

    checked player value for first and second and also check the name for players

--- spec/game_test_spec.rb
+++ spec/game_test_spec.rb
@@ -27,5 +27,28 @@ RSpec.describe GameUi do
       end
     end
   end
-
+  context 'asking for the player value' do
+    it 'will ask for the value for the first player' do
+      allow(maintitle).to receive(:gets).and_return('name')
+      expect { maintitle.ask_player_for_value }.to output("Please choose between X or O \n").to_stdout
+    end
+  end
+  describe 'ask the name for the second player' do
+    it 'will ask the second player for their name' do
+      allow(maintitle).to receive(:gets).and_return('name')
+      expect { maintitle.ask_player_for_name(2) }.to output("Please provide the name for Player2: \n").to_stdout
+    end
+  end
+  describe 'validate the selection made by the first player' do
+    it 'validate the choice made' do
+      expect(maintitle.validating_value('X')).to eq('X')
+    end
+  end
+  context 'validate if they inmput the wrong choice' do
+    it 'will validate if they provide the wrong input' do
+      allow(maintitle).to receive(:gets).and_return('X')
+      expect { maintitle.validating_value('K') }.to output("Please enter X or O\n").to_stdout
+    end
+  end
+  
 end
